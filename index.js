0a3c0a308a6c701a6cd68511df72a5958123a8dd
Daniel Dabah <63557862+ddabahc@users.noreply.github.com>
2022-01-03 15:41:03 -0500

    WDPT MX DANIEL DABAH
    HICE UN ERROR Y NO PUDE HACER COMMIT DESDE LA TERMINAL TUVE QUE SUBIRLOS POR UPLOAD

--- index.js
+++ index.js
@@ -5,17 +5,77 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost/recipe-app';
+
+const oneRecipe = {
+  title: "Pie",
+  level: "Easy Peasy",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+  ],
+  cuisine: "American",
+  dishType: "dessert",
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  duration: 40,
+  creator: "Chef Carlos",
+};
+
+const manyRecipes = [
+  {
+    title: "Pie 2",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+  {
+    title: "Pie 3",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+  {
+    title: "Pie 4",
+    level: "Easy Peasy",
+    ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+    ],
+    cuisine: "American",
+    dishType: "dessert",
+    image: "https://images.media-allrecipes.com/images/75131.jpg",
+    duration: 40,
+    creator: "Chef Carlos",
+  },
+];
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const recipeSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    level: { type: String },
+    ingredients: { type: [String] },
+    cuisine: { type: String },
+    dishType: { type: String },
+    image: {
+      type: String,
+      default: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.669xw:1.00xh;0.173xw,0&resize=640:*",
+    },
+    duration: { type: Number },
+    creator: { type: String },
+  },
+  {
+    timestamp: true,
+  }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
